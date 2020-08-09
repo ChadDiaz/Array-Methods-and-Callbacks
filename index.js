24e62006010461864e284f31811f8d5b3edb223b
ChadDiaz <chadcdiaz@gmail.com>
2020-08-08 23:24:41 -0400

    through task 4

--- index.js
+++ index.js
@@ -1,7 +1,8 @@
 import { fifaData } from './fifa.js';
-console.log(fifaData);
+// console.log(fifaData);
 
-console.log('its working');
+// console.log('its working');
+console.log("");
 // ⚽️ M  V P ⚽️ //
 
 /* Task 1: Investigate the data above. Practice accessing data by console.log-ing the following pieces of data 
@@ -11,35 +12,71 @@ console.log('its working');
 (c) Home Team goals for 2014 world cup final
 (d) Away Team goals for 2014 world cup final
 (e) Winner of 2014 world cup final */
+console.log ("*******   TASK 1 ANSWERS   *********")
+let team = fifaData.filter(element => element.Year === 2014);
+for (let i = 0; i< team.length; i++){
+    if (team[i]["Stage"] === "Final"){
+        console.log("The Task 1a answer is " + team[i]["Home Team Name"]);
+    }
+    if (team[i]["Stage"] === "Final") {
+        console.log("The Task 1b answer is " + team[i]["Away Team Name"]);
+    }
+    if (team[i]["Stage"] === "Final"){
+        console.log("The Task 1c answer is " + team[i]["Home Team Goals"]);
+    }
+    if (team[i]["Stage"] === "Final"){
+        console.log("The Task 1d answer is " + team[i]["Away Team Goals"]);
+    }
+    if (team[i]["Stage"] === "Final"){
+        console.log("The Task 1e answer is " + team[i]["Win conditions"]);
+    }
+}
+console.log("");
+
+
 
 
 /* Task 2: Create a function called  getFinals that takes `data` as an argument and returns an array of objects with only finals data */
 
-function getFinals(/* code here */) {
-
-    /* code here */
-
+function getFinals(data) {
+    const finals = data.filter(final => {
+        return final["Stage"] == "Final"
+    })   
+    return (finals);
 };
+console.log("The answer to task 2 is " , getFinals(fifaData));
+console.log("");
 
 /* Task 3: Implement a higher-order function called `getYears` that accepts the callback function `getFinals`, and returns an array called `years` containing all of the years in the dataset */
 
-function getYears(/* code here */) {
-
-    /* code here */
-
+function getYears(callBack) {
+    let data = callBack(fifaData);
+    let years = [];
+    data.forEach((item) => {
+        years.push(item.Year);
+    })
+   return years;
 };
 
-getYears();
+console.log("The answer to task 3 is " , getYears(getFinals));
 
 /* Task 4: Implement a higher-order function called `getWinners`, that accepts the callback function `getFinals()` and determine the winner (home or away) of each `finals` game. Return the name of all winning countries in an array called `winners` */ 
 
-function getWinners(/* code here */) {
-
-    /* code here */
-
+function getWinners(callBack) {
+    const data = callBack(fifaData);
+    const winners = [];
+    data.forEach ((item) => {
+        if (item["Home Team Goals"] > item["Away Team Goals"]) {
+            winners.push(item["Home Team Name"]);
+        }else{
+            winners.push(item["Away Team Name"]);
+        }
+    })
+    return winners;
 };
 
-getWinners();
+console.log("The asnwer to task 4 is " , getWinners(getFinals));
+console.log("");
 
 /* Task 5: Implement a higher-order function called `getWinnersByYear` that accepts the following parameters and returns a set of strings "In {year}, {country} won the world cup!" 
 
@@ -48,11 +85,17 @@ Parameters:
  * callback function getYears
  */
 
-function getWinnersByYear(/* code here */) {
+function getWinnersByYear(cb1, cb2) {
+    const winners = cb1(getFinals);
+    const years = cb2(getFinals);
 
-};
-
-getWinnersByYear();
+    const winningString= winners.forEach((item, index) =>{
+        console.log("The answer to task 5 is: " + `"In ${years[index]}, ${item} won the World Cup!"`);
+});
+return winningString;
+}
+(getWinnersByYear(getWinners, getYears));
+console.log("");
 
 /* Task 6: Write a function called `getAverageGoals` that accepts a parameter `data` and returns the the average number of home team goals and away team goals scored per match (Hint: use .reduce and do this in 2 steps) */
 
